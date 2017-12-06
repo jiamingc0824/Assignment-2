54e89d876982f604e956f97ab29c42a2a03bf2a5
Jia Ming <jia.ming.0824@hotmail.com>
2017-12-06 10:39:22 +0800

    Added comments and Speak() function fixed

--- 017031/task.cpp
+++ 017031/task.cpp
@@ -3,71 +3,79 @@
 #include <vector>
 using namespace std;
 
-class Animal
+class Animal//Declaration of Animal class
 {
 public:
-	Animal(string name, int age);
-	string getName();
-	int getAge();
-	void setName(string name);
-	void setAge(int age);
-	virtual void Speak();
-	virtual void Action();
-protected:
-	string Name;
-	int Age;
+	Animal()=default;//Default constructor for class Animal
+	Animal(string name, int age);//Constructor overloading for class Animal
+	string getName();//Getter function for Name
+	int getAge();//Getter function for Age
+	void setName(string name);//Setter function for Name
+	void setAge(int age);//Setter function for Age
+	virtual void Speak();//Virtual function Speak which can be overridden
+	virtual void Action();//Virtual function Action which can be overridden
+	virtual void Introduce();//Virtual function Introduce which can be overridden
+private:
+	string Name;//Name property of class Animal
+	int Age;//Age property of class Animal
 
 };
-Animal::Animal(string name, int age)
+
+Animal::Animal(string name, int age)//Function definition of overloaded constructor of class Animal
 {
-	Name = name;
-	Age = age; 
+	setAge(age);
+	setName(name); 
 }
-string Animal::getName()
+string Animal::getName()//Getter function for Name
 {
 	return Name;
 }
 
-void Animal::setName(string name)
+void Animal::setName(string name)//Setter function for Name
 {
 	Name = name;
 }
-int Animal::getAge()
+int Animal::getAge()//Getter function for Age
 {
 	return Age;
 }
-void Animal::setAge(int age)
+void Animal::setAge(int age)//Setter function for Age
 {
 	Age = age;
 }
-void Animal::Speak()
-{
-	//empty
-}
-void Animal::Action()
-{
-	//empty
-}
-
-
-
-class Dog :public Animal
-{
-public:
-	Dog(string name, int age);
-	virtual void Speak();
-	virtual void Action();
-};
-
-Dog::Dog(string name, int age):Animal(name, age)
+void Animal::Speak() //Function definition for Speak
 {
 	//Empty
 }
-void Dog::Speak()
+void Animal::Action() //Function definition for Action
+{
+	//Empty
+}
+
+void Animal::Introduce()//Function definition of Introduce
+{
+	cout <<"I am "<< getName()<<"and I am "<<Age<<" years old" << endl;
+}
+
+
+class Dog :public Animal//Declaration of class Dog with public inherited from class Animal
+{
+public:
+	Dog()=default;//Default constructor for class Dog
+	Dog(string name, int age);//Constructor overloading for class Dog
+	virtual void Speak();//Virtual function Speak which can be overridden
+	virtual void Action();//Virtual function Action which can be overridden
+};
+
+Dog::Dog(string name, int age):Animal(name, age)//Function definition of overloaded constructor of class Dog
+{
+	//Copying class Dog data to class Animal
+}
+void Dog::Speak()//Function definition for overridden Speak
 {
 	cout << "Bark" << endl;
 }
-void Dog::Action()
+void Dog::Action()//Function definition for overridden Action
 {
 	cout << "Rawr" << endl;
 }
@@ -75,81 +83,97 @@ void Dog::Action()
 
 
 
-class PetDog :public Dog
+class PetDog :public Dog//Declaration of class PetDog with public inherited from class Dog
 {
 public:
-	PetDog(string name, int age);
-	void Speak() override;
-	void Action() override;
+	PetDog()=default;//Default constructor for class PetDog
+	PetDog(string name, int age);//Constructor overloading for class PetDog
+	void Speak() override;//Overriding Speak function
+	void Action() override;//Overriding Action function
 };
-PetDog::PetDog(string name, int age) :Dog(name, age)
+PetDog::PetDog(string name, int age) :Dog(name, age)//Function definition of overloaded constructor of class PetDog
 {
-
+	//Copying class PetDog data to class Dog
 }
-void PetDog::Speak()
+void PetDog::Speak()//Function definition for overridden Speak
 {
 	cout << "BarkBark" << endl;
 }
-void PetDog::Action()
+void PetDog::Action()//Function definition for overridden Action
 {
 	cout << "Fetch a stick" << endl;
 }
 
 
-class StrayDog :public Dog
+class StrayDog :public Dog//Declaration of class StrayDog with public inherited from class Dog
 {
 public:
-	StrayDog(string name, int age);
-	void Speak() override;
-	void Action() override;
+	StrayDog()=default;//Default constructor for class StrayDog
+	StrayDog(string name, int age);//Constructor overloading for class StrayDog
+	void Speak() override;//Overriding Speak function
+	void Action() override;//Overriding Action function
 };
-StrayDog::StrayDog(string name, int age) :Dog(name, age)
+StrayDog::StrayDog(string name, int age) :Dog(name, age)//Function definition of overloaded constructor of class Dog
 {
-
+	//Copying class StrayDog data to class Dog
 }
-void StrayDog::Speak()
+void StrayDog::Speak()//Function definition for overridden Speak
 {
 	cout << "woof woof" << endl;
 }
-void StrayDog::Action()
+void StrayDog::Action()//Function definition for overridden Action
 {
 	cout << "Chase Cars" << endl;
 }
 
 
 
-class Cat :public Animal
+class Cat :public Animal//Declaration of class Cat with public inherited from class Animal
 {
 public:
-	Cat(string name, int age);
-	void Speak() override;
-	void Action() override;
+	Cat()=default;//Default constructor for class Cat
+	Cat(string name, int age);//Constructor overloading for class Cat
+	void Speak() override;//Overriding Speak function
+	void Action() override;//Overriding Action function
 };
-Cat::Cat(string name, int age) :Animal(name, age)
+Cat::Cat(string name, int age) :Animal(name, age)//Function definition of overloaded constructor of class Cat
 {
-
+	//Copying class Cat data to class Animal
 }
-void Cat::Speak()
+void Cat::Speak()//Function definition for overridden Speak
 {
 	cout << "Meow" << endl;
 }
-void Cat::Action()
+void Cat::Action()//Function definition for overridden Action
 {
 	cout << "Playing with knit" << endl;
 }
 
-
 int main()
 {
-	vector<Animal> Animals;
-	Animals.push_back(Animal("James",10));
-	Animals.push_back(Animal("Doge", 10));
-	Animals.push_back(Animal("Cate", 10));
-	Animals.push_back(Animal("Random Dog", 10));
-	Animals.push_back(Animal("Lost Dog", 10));
-	Animals.push_back(Animal("Kot", 10));
-	Animals.push_back(Animal("Sobaka", 10));
+	int a;
+	Animal* Animals[7];
+	Animal* Random = &Animal("Sam", 10);
+	Animal* Random1 = &Animal("Tom", 12);
+	Dog* Doge = &Dog("Doggy", 13);
+	StrayDog* StrayDoge = &StrayDog("Sobaka", 5);
+	PetDog* PetDoge = &PetDog("Doge", 6);
+	PetDog* PetDoge1 = &PetDog("Calculus", 6);
+	Cat* Kris = &Cat("Kot", 5);
 
+	Animals[0] = Random;
+	Animals[1] = Random1;
+	Animals[2] = Doge;
+	Animals[3] = StrayDoge;
+	Animals[4] = PetDoge;
+	Animals[5] = PetDoge1;
+	Animals[6] = Kris;
 
+	Kris->Introduce();
+	for (a = 0; a < 7; a++)
+	{
+		Animals[a]->Introduce();
+		Animals[a]->Speak();
+	}
 	system("pause");
 }
